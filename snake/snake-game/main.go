bc4ace8ec7c4f2de392338aff01067b2248f470b
Quinn-McClure <100171762+Quinn-McClure@users.noreply.github.com>
2026-05-04 12:38:00 -0500

    grid and square

--- snake/snake-game/main.go
+++ snake/snake-game/main.go
@@ -1,28 +1,61 @@
 package main
 
 import (
+	"image/color"
+	"log"
+
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/vector"
 )
 
 const (
 	screenWidth = 640
 	screenHeight = 480
+	gridSize = 20
 )
 
-type Game struct {}
+type Point struct {
+	x, y int
+}
+
+type Game struct {
+	snake []Point
+}
 
 func (g *Game) Update() error {
 	return nil
 }
 
-func (g *Game) Draw(screen, *ebiten.Image) {}
+func (g *Game) Draw(screen *ebiten.Image) {
+	for _, p := range g.snake {
+		vector.FillRect(
+			screen, 
+			float32(p.x * gridSize), 
+			float32(p.y * gridSize), 
+			float32(gridSize), 
+			float32(gridSize), 
+			color.White, 
+			true,
+		)
+	}
+}
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 	return screenWidth, screenHeight
 }
 
 func main() {
-	g := &Game{}
+	//initialization of game object
+	g := &Game{
+		snake: []Point{{
+			x: screenWidth / gridSize / 2, 
+			y: screenHeight / gridSize/ 2,
+		}},
+	}
 
-	ebiten.RunGame(g)
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+	ebiten.SetWindowTitle("Snake")
+	if err := ebiten.RunGame(g); err != nil {
+		log.Fatal(err)
+	}
 }
