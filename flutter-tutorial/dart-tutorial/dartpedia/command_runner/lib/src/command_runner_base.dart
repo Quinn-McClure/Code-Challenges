49b4ca3ac01213c68124cc2650096005ef6ec77f
Quinn-McClure <100171762+Quinn-McClure@users.noreply.github.com>
2026-06-01 11:34:27 -0500

    in middle of lesson 7

--- flutter-tutorial/dart-tutorial/dartpedia/command_runner/lib/src/command_runner_base.dart
+++ flutter-tutorial/dart-tutorial/dartpedia/command_runner/lib/src/command_runner_base.dart
@@ -1,19 +1,33 @@
+import 'dart:async'; 
 import 'dart:collection';
 import 'dart:io';
 import 'arguments.dart';
+import 'exceptions.dart';
 
 class CommandRunner {
+  CommandRunner({this.onError});
+  
   final Map<String, Command> _commands = <String, Command>{};
 
   UnmodifiableSetView<Command> get commands => 
     UnmodifiableSetView<Command>(<Command>{..._commands.values});
 
+  FutureOr<void> Function(Object)? onError;
+
   Future<void> run(List<String> input) async {
-    final ArgResults results = parse(input);
+    try {
+      final ArgResults results = parse(input);
     
-    if(results.command != null) {
-      Object? output = await results.command!.run(results);
-      print(output.toString());
+      if(results.command != null) {
+        Object? output = await results.command!.run(results);
+        print(output.toString());
+      }
+    } on Exception catch (exception) {
+      if (onError != null) {
+        onError!(exception);
+      } else {
+        rethrow;
+      }
     }
   }
 
@@ -24,9 +38,53 @@ class CommandRunner {
   }
 
   ArgResults parse(List<String> input) {
-    var results = ArgResults();
-    results.command = _commands[input.first];
-    return results;
+    ArgResults results = ArgResults();
+    if (input.isEmpty) return results;
+
+    if(_commands.containsKey(input.first)) {
+      results.command = _commands[input.first];
+      input = input.sublist(1);
+    } else {
+      throw ArgumentException(
+        'The first word of input must be a command.',
+        null,
+        input.first,
+      );
+    }
+
+    if (results.command != null && input.isNotEmpty && _commands.containsKey(input.first)) {
+      throw ArgumentException(
+        'Input can only contain one command. Got ${input.first} and ${results.command!.name}',
+        null,
+        input.first,
+      );
+    }
+
+    Map<Option, Object?> inputOptions = {};
+    int i = 0;
+    while (i < input.length) {
+      if (input[i].startsWith('-')) {
+        var base = _removeDash(input[i]);
+        var option = results.command!.options.firstWhere(
+          (option) => option.name == base || option.abbr == base,
+          orElse: () {
+            throw ArgumentException(
+              'Unknown option ${input[i]}',
+              results.command!.name,
+              input[i],
+            );
+          },
+        );
+      }
+
+      if (option.type == OptionType.flag) {
+        inputOptions[option] = true;
+        i++;
+        continue;
+      }
+    }
+
+
   }
 
   // Returns usage for the executable only.
