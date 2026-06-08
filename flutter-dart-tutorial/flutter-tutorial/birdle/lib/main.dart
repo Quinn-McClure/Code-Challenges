b5d67ca093a1a9e307d66b832dae17b70e375ece
Quinn-McClure <100171762+Quinn-McClure@users.noreply.github.com>
2026-06-08 11:31:30 -0500

    catch up

--- flutter-dart-tutorial/flutter-tutorial/birdle/lib/main.dart
+++ flutter-dart-tutorial/flutter-tutorial/birdle/lib/main.dart
@@ -11,10 +11,16 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          title: const Align(
+            alignment: Alignment.centerLeft,
+            child: Text('Birdle'),
+          ),
+        ),
         body: Center(
-          child: Tile('A', HitType.hit),
+          child: Center(child: GamePage()),
         ),
       ),
     );
@@ -47,6 +53,30 @@ class Tile extends StatelessWidget {
           style: Theme.of(context).textTheme.titleLarge,
         ),
       ),
+    );
+  }
+}
+
+class GamePage extends StatelessWidget {
+  GamePage({super.key});
+  final Game _game = Game();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column (
+        spacing: 5.0,
+        children: [
+          for (final guess in _game.guesses)
+            Row (
+              spacing: 5.0,
+              children: [
+                for(final letter in guess) Tile(letter.char, letter.type),
+              ],
+            )
+        ],
+      )
     );
   }
 }
